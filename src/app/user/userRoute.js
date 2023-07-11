271d2743c3c7af875cb120c70edf288e9257540e
JJong <63280700+entere77@users.noreply.github.com>
2023-07-11 18:59:43 +0900

    [ADD] userRoute.js 문자 인증 전송 및 검증 추가

--- src/app/user/userRoute.js
+++ src/app/user/userRoute.js
@@ -1,9 +1,9 @@
 module.exports = function (app) {
     const user = require("./userController");
   
-    //1. 유저 생성 api
-    app.post("/app/users", user.sign_up);
-    
-    
+    // 문자 인증 전송 API
+    app.post('/users/send', user.send);
+    // 문자 인증 검증 API
+    app.post('/users/verify', user.verify);
+     
   };
-  
