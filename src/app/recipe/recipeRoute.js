373fb114a6bd09e94c7cca62d43924cf1d471be9
himinsunsine <himinsunsine@naver.com>
2023-07-20 12:57:03 +0900

    [ADD]recipeDao에서 여러 쿼리 추가

--- src/app/recipe/recipeRoute.js
+++ src/app/recipe/recipeRoute.js
@@ -3,6 +3,9 @@ module.exports = function (app) {
   
     //15. 레시피의 리뷰(후기) 등록하기
     app.post("/recipe/:recipe_id/review/:userid", recipe.PostRegisterReview);
+
+    //20. 레시피 상세 페이지 조회
+    app.get("/recipe/detail", recipe.GetDetail);
     
     //34. 전체 레시피 조회
     app.get("/recipe", recipe.GetallRecipe);
