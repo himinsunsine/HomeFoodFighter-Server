8db8e9076609ce6290a3692be688b957331e3583
funfungun <alpha5179@naver.com>
2023-07-20 19:23:13 +0900

    [MOD] refrigerator 코드작성중

---
+++ src/app/refrigerator/refrigeratorRoute.js
@@ -0,0 +1,13 @@
+module.exports = function(app){
+    const refrigerator = require("./refrigeratorController");
+    const jwtMiddleware = require('../../../config/jwtMiddleware');
+
+    // 22. 냉장고 조회 API
+    app.get('/refrigerator', refrigerator.inquire);
+
+    // 23. 냉장고 채우기 API
+    app.post('/refrigerator/fill', refrigerator.fill);
+     
+    // 24. 냉장고 비우기 API
+    app.delete('/refrigerator/empty', refrigerator.empty)
+  };
