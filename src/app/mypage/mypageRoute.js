245fce969ece96703222f6759dd7ecc5a6fbcbe9
JJong <63280700+entere77@users.noreply.github.com>
2023-07-22 13:51:30 +0900

    [FEAT] 비밀번호 변경 기능 구현

--- src/app/mypage/mypageRoute.js
+++ src/app/mypage/mypageRoute.js
@@ -1,7 +1,11 @@
 module.exports = function (app) {
-    const mypage = require("./mypageController");
+  const jwtMiddleware = require('../../../config/jwtMiddleware');
+  const mypage = require("./mypageController");
+    
+  //4. 비밀번호 변경 API
+  app.post('/mypages/password', jwtMiddleware, mypage.patchPassword);    
 
-    // //9. 찜한 레시피 조회
-    // app.post("/mypage/favorite", mypage.);    
+  // //9. 찜한 레시피 조회
+  // app.post("/mypage/favorite", mypage.);
      
-  };
+};
--- src/app/mypage/mypageService.js
+++ src/app/mypage/mypageService.js
@@ -1,10 +1,32 @@
 const {logger} = require("../../../config/winston");
 const {pool} = require("../../../config/database");
-const userProvider = require("./mypageProvider");
-const userDao = require("./mypageDao");
+const mypageProvider = require("./mypageProvider");
+const mypageDao = require("./mypageDao");
 const baseResponse = require("../../../config/baseResponse");
 const {response} = require("../../../config/response");
 const {errResponse} = require("../../../config/response");
-
+const jwtMiddleware = require("../../../config/jwtMiddleware");
 const crypto = require("crypto");
 const {connect} = require("http2");
+
+exports.editPassword = async function (password_present, password_new, userid,) {
+    try {
+        // 비밀번호 존재 확인
+        const passwordRows = await mypageProvider.passwordCheck(password_present, userid);
+        if (passwordRows.length == 0)
+            return errResponse(baseResponse.EDITING_PASSWORD_PRESENT_WRONG);
+
+        const updatePasswordInfoParams = [password_new, userid];
+        const connection = await pool.getConnection(async (conn) => conn);
+        const editPasswordResult = await mypageDao.updatePasswordInfo(connection, updatePasswordInfoParams);
+
+        console.log(`${userid}의 비밀번호 수정 완료`);
+        connection.release();
+
+        return response(baseResponse.SUCCESS);
+
+    } catch (err) {
+        logger.error(`App - editUser Service error\n: ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+    }
+}
