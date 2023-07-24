fcf3a2b8aa64f77cf694e1bff571ff7d201f1232
himinsunsine <himinsunsine@naver.com>
2023-07-24 23:16:12 +0900

    [FEAT] api.16 주간캘린더 조회 완성

--- src/app/calendar/calendarController.js
+++ src/app/calendar/calendarController.js
@@ -3,3 +3,16 @@ const calendarService = require("../../app/calendar/calendarService");
 const baseResponse = require("../../../config/baseResponse");
 const { response, errResponse } = require("../../../config/response");
 const jwtMiddleware = require('../../../config/jwtMiddleware');
+
+/**
+ * API No. 16
+ * API Name : 주간 캘린더 조회
+ * [GET] /calendar/week
+ */
+exports.GetWeek = async function (req, res) {
+    const userid = req.verifiedToken.userId;
+
+    const weekInfoResult = await calendarProvider.getWekekInfo(userid);
+    return res.send(weekInfoResult);
+
+};
--- src/app/calendar/calendarDao.js
+++ src/app/calendar/calendarDao.js
@@ -1,1 +1,14 @@
 const baseResponse = require("../../../config/baseResponse");
+
+//API.16 주간 캘린더 조회
+async function getWeek(connection, userid) {
+    
+    const getWeekQuery = `
+    SELECT userid,recipe_id,bydate,meal_time FROM Calendar WHERE userid = ${userid} AND WEEK(bydate, 1) = WEEK(CURDATE(), 1);
+    `;
+    const [weekRows] = await connection.query(getWeekQuery);
+    return weekRows;
+}
+module.exports={
+    getWeek,
+}
